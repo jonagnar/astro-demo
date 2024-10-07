dc6cbdfb323de6c468a91f2df579b6e055d790dc
JonnXor <jonnxor@gmail.com>
2024-10-07 10:30:04 +0000

    feat: tailwind support

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap(), tailwind()]
 });
